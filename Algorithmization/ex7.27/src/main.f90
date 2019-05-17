ead03adef11074a1f9c7925e77f1446643ea3bbc
Mikhail <qr.stronger@gmail.com>
2019-05-17 10:52:19 +0300

    ex7.27 refactoring

--- Algorithmization/ex7.27/src/main.f90
+++ Algorithmization/ex7.27/src/main.f90
@@ -4,38 +4,30 @@ program exercise_7_27
    implicit none
    character(*), parameter :: input_file = "../data/input.txt", output_file = "output.txt"
    integer                 :: In = 0, Out = 0, N = 0, M = 0, i
-   real(R_), allocatable   :: B(:,:)
+   real(R_), allocatable   :: B(:,:), tmp(:)
 
    ! Ввод данных.
    open (file=input_file, newunit=In)
-      read (In, *) N, M
+      read (In, *) M, N
       allocate (B(M, N))
       read (In, *) B
    close (In)
+   allocate (tmp(M))
+
+   open (file=output_file, encoding=E_, newunit=Out)
+       write (Out, '('//M//'f6.2)') B
+   close (Out)
 
    do concurrent (i = 1:M-1:2)
-      B(i, :) = B(i, :) + B(i+1, :)
-      B(i+1, :) = B(i, :) - B(i+1, :)
-      B(i, :) = B(i, :) - B(i+1, :)
+         tmp = B(i, :)
+         B(i, :) = B(i+1, :)
+         B(i+1, :) = tmp
    end do
 
    ! Вывод данных.
-   open (file=output_file, encoding=E_, position='rewind', newunit=Out)
+   open (file=output_file, encoding=E_, position='append', newunit=Out)
+       write (Out, *) "После перестановки:"
        write (Out, '('//M//'f6.2)') B
    close (Out)
    
-contains
-   pure subroutine Swap(B, M)
-      real(R_), intent(inout) :: B(:, :)
-      integer, intent(in)     :: M
-
-      integer i
-
-      do concurrent (i = 1:M-1:2)
-         B(i, :) = B(i, :) + B(i+1, :)
-         B(i+1, :) = B(i, :) - B(i+1, :)
-         B(i, :) = B(i, :) - B(i+1, :)
-      end do
-
-   end subroutine Swap
 end program exercise_7_27

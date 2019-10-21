ade788fdf18c331b0f3298d1fb54ec469769b108
Mikhail <qr.stronger@gmail.com>
2019-10-21 14:49:25 +0300

    C++ lab1 refactoring

--- C++/lab1/main.cpp
+++ C++/lab1/main.cpp
@@ -1,9 +1,27 @@
 #include <iostream>
 
+char *customStrncat(char * __restrict dst, const char * __restrict src, size_t n){
+    if (n != 0) {
+        char *d = dst;
+        const char *s = src;
+
+        while (*d != 0)
+            d++;
+        do {
+            if ((*d = *s++) == 0)
+                break;
+            d++;
+        } while (--n != 0);
+        *d = 0;
+    }
+    return (dst);
+}
+
 int main() {
     char string1[] = {'H', 'e', 'l', 'l', 'o'};
     char string2[] = {' ', 'w', 'o', 'r', 'l', 'd'};
-    std::cout << strncat(string1, string2, 6);
+    //std::cout << strncat(string1, string2, 6);
+    std::cout << customStrncat(string1, string2, 6);
 
     return 0;
 }

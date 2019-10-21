32674f9d4b3a88b96cae99544b39b56e2ba34957
Mikhail <qr.stronger@gmail.com>
2019-10-21 15:08:39 +0300

    C++ lab3-4 is done

---
+++ C++/lab3-4/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+char *customStrncat(char * __restrict dst, const char * __restrict src, size_t n){
+    if (n != 0) {
+        char *d = dst;
+        const char *s = src;
+
+        while (*d != 0)
+            d++;
+        do {
+            if ((*d = *s++) == 0)
+                break;
+            d++;
+        } while (--n != 0);
+        *d = 0;
+    }
+    return (dst);
+}
+
+int main(int argc, char *argv[]) {
+    int n = atoi(argv[3]);
+    std::cout << customStrncat(argv[1], argv[2], n);
+
+    return 0;
+}
